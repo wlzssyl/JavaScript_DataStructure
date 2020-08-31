77c4a17526a067158af3dfa3f1c3aa7ab6c2a035
wlzssyl <zhangboy7660@qq.com>
2020-08-31 18:58:05 +0800

    链表初始

---
+++ linked_list/linked_list.js
@@ -0,0 +1,22 @@
+class Node {
+  constructor(element) {
+    this.element = element;
+    this.next = null;
+  }
+}
+
+class linkedList {
+  constructor() {
+    this.head = null;
+    this.length = 0;
+  }
+  //append(element);向列表尾部增加新项
+  //insert(position, element); 向链表特定位置插入新的项
+  //get(position); 获取对应位置处的元素
+  //indexof(element); 返回元素再链表中的位置，没有则返回-1
+  //update(position, element); 修改某个位置的元素
+  //removeAt(position); 从链表特定位置移除一项
+  //remove(element); 从链表中移出一项
+  //isEmpty(); 判断链表长度是否为0，为0返回true
+  //size(); 返回链表元素个数
+}
