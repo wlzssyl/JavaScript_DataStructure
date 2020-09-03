ced01bb2e1340144516aa93daf15131b784b4f24
wlzssyl <zhangboy7660@qq.com>
2020-09-03 17:41:53 +0800

    双向链表3

--- linked_list/double_linkedList.js
+++ linked_list/double_linkedList.js
@@ -10,6 +10,107 @@ class linkedList {
     this.head = null;
     this.length = 0;
   }
+  //append(element);向列表尾部增加新项
+  append(element) {
+    let newNode = new Node(element);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+    this.length++;
+  }
+  //insert(position, element); 向链表特定位置插入新的项
+  insert(position, element) {
+    if(position < 0 || position > this.length) return false;
+    let newNode = new Node(element);
+    if (position === 0) {
+      newNode.next = this.head.next;
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      let previous = null;
+      let i = 0;
+      while (i++ < position) {
+        previous = current;
+        current = current.next;
+      }
+      //插入新节点
+      previous.next = newNode;
+      newNode.next = current;
+    }
+    this.length++;
+  }
+  //get(position); 获取对应位置处的元素
+  get(position) {
+    if(position < 0 || position > this.length-1) return false;
+    let index = 0;
+    let current = this.head;
+    if(position === 0){
+      return current;
+    }else{
+      while(index++ < position){
+        current = current.next;
+      }
+      return current;
+    }
+  }
+  //indexof(element); 返回元素再链表中的位置，没有则返回-1
+  indexOf(element) {
+    let current = this.head;
+    let position = 0;
+    while(current){
+      if(element == current.element){
+        return position;
+      }else{
+        current = current.next;
+        position++;
+      }
+      if(position > this.length){
+        reutrn -1;
+      }
+    }
+  }
+  //removeAt(position); 从链表特定位置移除一项
+  removeAt(position) {
+    if(position < 0 || position > this.length-1) return false;
+    const current = this.get(position);
+    if(position === 0){
+      this.head = current.next;
+      this.length--;
+    }else{ 
+      const previous = this.get(position-1);
+      previous.next = current.next;
+      this.length--;
+    }
+  }
+  //update(position, element); 修改某个位置的元素
+  update(position, element) {
+    if(position < 0 || position > this.length-1) return false;
+    const current = this.get(position);
+    current.element = element;
+  }
+  //remove(element); 从链表中移出一项
+  remove(element){
+    const position = this.indexOf(element);
+    this.removeAt(position);
+  }
+  //isEmpty(); 判断链表长度是否为0，为0返回true
+  isEmpty() {
+    if(this.length){
+      return false;
+    }else{
+      return true;
+    }
+  }
+  //size(); 返回链表元素个数
+  size() {
+    return this.length;
+  }
 }
 //*********************************** */
 //双向链表节点
@@ -70,9 +171,13 @@ class doubelLinkedList extends linkedList {
 
     this.length++;
   }
-  //get(position); 获取对应位置的节点
-  //indexOf(element); 返回元素所在链表中的位置索引，没有的话返回-1
+  /******************************************** 
+  get(position); 获取对应位置的节点
+  indexOf(element); 返回元素所在链表中的位置索引，没有的话返回-1
+  get,indexOf方法继承自单向链表类
+  *******************************************/
   //update(position, element); 修改某个位置的元素
+
   //removeAt(position); 在链表指定位置删除节点
   //remove(element); 在链表中删除该元素的节点
   //isEmpty(); 链表为空的话，返回true
@@ -87,3 +192,7 @@ console.log(doubleList);
 
 doubleList.insert(1,101);
 doubleList.insert(3,202);
+console.log(doubleList);
+
+console.log(doubleList.get(3));
+console.log(doubleList.indexOf(333));
