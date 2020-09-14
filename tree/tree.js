2fc602ef9856c8c81edcc1c83c7c6a408173919d
wlzssyl <zhangboy7660@qq.com>
2020-09-14 15:21:26 +0800

    树2

--- tree/tree.js
+++ tree/tree.js
@@ -1,3 +1,13 @@
 class Node {
-  
+  constructor() {
+    this.key = key;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.tree = null;
+  }
 }
