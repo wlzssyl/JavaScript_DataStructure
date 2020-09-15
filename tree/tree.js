ce3631206e832d9af5ccad4ed7c8da5fcf68816f
wlzssyl <zhangboy7660@qq.com>
2020-09-15 15:39:42 +0800

    二叉树插入节点

--- tree/tree.js
+++ tree/tree.js
@@ -9,9 +9,34 @@ class Node {
 class BinarySearchTree {
   constructor() {
     this.tree = null;
+    this.root = null;
   }
 
   //insert(key),向树中插入新的键
+  insert(key) {
+    const newNode = new Node(key);
+    if(!this.root){
+      this.root = newNode;
+    }else{
+      this.insertNewNode(this.root, newNode);
+    }
+  }
+  insertNewNode(root, newNode){
+    if(root.key > newNode.key){
+      if(!root.left){
+        root.left = newNode;
+      }else{
+        this.insertNewNode(root.left, newNode);
+      }
+    }else{
+      if(!root.right){
+        root.right = newNode;
+      }else{
+        this.insertNewNode(root.right, newNode);
+      }
+    }
+  }
+
   //search(key),在树中查找一个键，如果节点存在，则返回true，否则返回false
   //inOrderTraverse, 中序遍历所有节点
   //preOrderTraverse, 先序遍历所有节点
@@ -20,3 +45,22 @@ class BinarySearchTree {
   //max,返回树中最大的键值
   //remove(key), 从树中移除某个键
 }
+/****************************************************** */
+
+const bst = new BinarySearchTree();
+bst.insert(11)
+bst.insert(7)
+bst.insert(15)
+bst.insert(5)
+bst.insert(3)
+bst.insert(9)
+bst.insert(8)
+bst.insert(10)
+bst.insert(13)
+bst.insert(12)
+bst.insert(14)
+bst.insert(20)
+bst.insert(18)
+bst.insert(25)
+bst.insert(6)
+console.log(bst)
