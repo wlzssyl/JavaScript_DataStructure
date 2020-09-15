a115a292c64a8906274c94194d888271d4e5406a
wlzssyl <zhangboy7660@qq.com>
2020-09-15 15:09:26 +0800

    树3

--- tree/tree.js
+++ tree/tree.js
@@ -1,5 +1,5 @@
 class Node {
-  constructor() {
+  constructor(key) {
     this.key = key;
     this.left = null;
     this.right = null;
@@ -10,4 +10,13 @@ class BinarySearchTree {
   constructor() {
     this.tree = null;
   }
+
+  //insert(key),向树中插入新的键
+  //search(key),在树中查找一个键，如果节点存在，则返回true，否则返回false
+  //inOrderTraverse, 中序遍历所有节点
+  //preOrderTraverse, 先序遍历所有节点
+  //postOrderTraverse, 后序遍历所有节点
+  //min,返回树中最小的键值
+  //max,返回树中最大的键值
+  //remove(key), 从树中移除某个键
 }
